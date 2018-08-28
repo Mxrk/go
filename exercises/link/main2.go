3b6c74e307d9397dcb12d87d711bf4fbd79792a9
Mxrk <Mxrk@users.noreply.github.com>
2018-08-28 22:32:02 +0200

    refactor, new try

---
+++ exercises/link/main2.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+
+	"golang.org/x/net/html"
+)
+
+type Link struct {
+	Href string
+	Text string
+}
+
+func main() {
+
+	s := `<html>
+	<head>
+	  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	</head>
+	<body>
+	  <h1>Social stuffs</h1>
+	  <div>
+		<a href="https://www.twitter.com/joncalhoun">
+		  Check me out on twitter
+		  <i class="fa fa-twitter" aria-hidden="true"></i>
+		</a>
+		<a href="https://github.com/gophercises">
+		  Gophercises is on <strong>Github</strong>!
+		</a>
+	  </div>
+	</body>
+	</html>`
+
+	doc, err := html.Parse(strings.NewReader(s))
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	var f func(*html.Node)
+	f = func(n *html.Node) {
+
+		var ret Link
+
+		ret.Href = (link(n))
+
+		ret.Text = (text(n))
+
+		fmt.Println(ret.Href)
+		fmt.Println(ret.Text)
+
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			f(c)
+		}
+
+	}
+	f(doc)
+
+}
+
+func text(n *html.Node) string {
+	if n.Type == html.TextNode {
+		return n.Data
+	}
+	if n.Type != html.ElementNode {
+		return ""
+	}
+	var ret string
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		ret += text(c)
+	}
+	return strings.Join(strings.Fields(ret), " ")
+}
+
+func link(n *html.Node) string {
+	for _, attr := range n.Attr {
+		if attr.Key == "href" {
+			return attr.Val
+		}
+	}
+	return ""
+}
